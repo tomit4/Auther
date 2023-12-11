92e7c1cd7da6c262a09aba61097495fe7188a4b8
tomit4 <mosssap@gmail.com>
2023-12-11 04:33:53 -0800

    :pencil2: Removed any type clause, allow for implied

--- backend/src/lib/utils/send-email.ts
+++ backend/src/lib/utils/send-email.ts
@@ -8,7 +8,7 @@ const apiInstance = new Brevo.TransactionalEmailsApi()
 /* TS-IGNORE: Property 'authentications' is protected and only accessible
  * within class 'TransactionalEmailsApi' and its subclasses. */
 // @ts-ignore
-const apiKey: any = apiInstance.authentications.apiKey
+const apiKey = apiInstance.authentications.apiKey
 apiKey.apiKey = String(process.env.BREVO_KEY)
 const sendSmtpEmail = new Brevo.SendSmtpEmail()
 
