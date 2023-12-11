6c21cb9806e8cb075ad6d201239c70e1b1c0aa76
tomit4 <mosssap@gmail.com>
2023-12-11 06:00:29 -0800

    :construction: Trying to address compiler complaints

--- backend/dist/lib/routes/index.js
+++ backend/dist/lib/routes/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// import { FastifyInstance, FastifyPluginCallback } from 'fastify'
+// import emailRoute from './onboarding/email'
 exports.default = async (fastify) => {
+    // await fastify.register(emailRoute, { prefix: '/onboarding' })
     // NOTE: using require as import throws overload ts error
-    // await fastify.register(import('./onboarding/email'))
     await fastify.register(require('./onboarding/email'), {
         prefix: '/onboarding',
     });
--- backend/src/lib/routes/index.ts
+++ backend/src/lib/routes/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
+// import { FastifyInstance, FastifyPluginCallback } from 'fastify'
+// import emailRoute from './onboarding/email'
 
 export default async (fastify: FastifyInstance): Promise<void> => {
+    // await fastify.register(emailRoute, { prefix: '/onboarding' })
     // NOTE: using require as import throws overload ts error
-    // await fastify.register(import('./onboarding/email'))
     await fastify.register(require('./onboarding/email'), {
         prefix: '/onboarding',
     })
