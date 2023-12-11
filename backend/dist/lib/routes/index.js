dc6dbc3394e6b67f691ed1cb4570a0697ca81589
tomit4 <mosssap@gmail.com>
2023-12-11 05:43:12 -0800

    :construction: Proper setting up of prefix on backend route

--- backend/dist/lib/routes/index.js
+++ backend/dist/lib/routes/index.js
@@ -3,5 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = async (fastify) => {
     // NOTE: using require as import throws overload ts error
     // await fastify.register(import('./onboarding/email'))
-    await fastify.register(require('./onboarding/email'));
+    await fastify.register(require('./onboarding/email'), {
+        prefix: '/onboarding',
+    });
 };
--- backend/src/lib/routes/index.ts
+++ backend/src/lib/routes/index.ts
@@ -3,5 +3,7 @@ import { FastifyInstance } from 'fastify'
 export default async (fastify: FastifyInstance): Promise<void> => {
     // NOTE: using require as import throws overload ts error
     // await fastify.register(import('./onboarding/email'))
-    await fastify.register(require('./onboarding/email'))
+    await fastify.register(require('./onboarding/email'), {
+        prefix: '/onboarding',
+    })
 }
