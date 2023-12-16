325a50edf15486d31d159b55070ca456a61bddd4
tomit4 <mosssap@gmail.com>
2023-12-16 10:02:00 -0800

    :pencil2: Removed unneeded ts compiled js file

--- backend/dist/db/migrations/20231216164014_test_migration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.down = exports.up = void 0;
-async function up(knex) {
-}
-exports.up = up;
-async function down(knex) {
-}
-exports.down = down;
