dfe0c5479301385c4a302e50dc725167e006a982
tomit4 <mosssap@gmail.com>
2023-12-17 04:20:42 -0800

    :memo: Note on how to proceed with knex plugin

--- backend/src/lib/types/index.ts
+++ backend/src/lib/types/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import type { Knex } from 'knex'
 
+// NOTE: there is a smarter way to do this, we need to look into creating a .d.ts file that more specifically binds knex to the fastifyInstance
+// https://github.com/fastify/fastify-redis/blob/master/types/index.d.ts
 type FastifyInstanceWithCustomPlugins = FastifyInstance & { knex?: Knex }
 
 export { FastifyInstanceWithCustomPlugins }
