d5a086909d98378bb214ac419b9bbf333c1cd5f0
tomit4 <mosssap@gmail.com>
2023-12-23 17:03:50 -0800

    :construction: Setting up auth proper in vue-router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+const authRoute = import.meta.env.VITE_AUTH_ROUTE
+const refreshRoute = import.meta.env.VITE_REFRESH_ROUTE
 
 import AppView from '../views/AppView.vue'
 import LoginView from '../views/LoginView.vue'
@@ -39,7 +41,7 @@ const routes = [
         path: '/app',
         component: AppView,
         name: 'AppView',
-        // meta: { requiresAuth: true},
+        meta: { requiresAuth: true },
     },
     {
         path: '/:catchAll(.*)*',
@@ -51,6 +53,31 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+// NOTE: not quite working yet
+router.beforeEach(async (to, from, next) => {
+    if (to.meta.requiresAuth && localStorage.getItem('appname-session-token')) {
+        const sessionToken = localStorage.getItem('appname-session-token')
+        const res = await fetch(authRoute, {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${sessionToken}` },
+        })
+        if (res.status === 401) {
+            const refreshCheck = await fetch(refreshRoute, {
+                method: 'GET',
+                credentials: 'include',
+                headers: { ContentType: 'application/json' },
+            })
+            if (refreshCheck.status !== 500) {
+                const jsonRes = await refreshCheck.json()
+                localStorage.setItem(
+                    'appname-session-token',
+                    jsonRes.sessionToken,
+                )
+            }
+        }
+    }
 })
 
 export { router }
--- frontend/src/views/AppView.vue
+++ frontend/src/views/AppView.vue
@@ -1,41 +1,4 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-const router = useRouter()
-
-const authRoute = import.meta.env.VITE_AUTH_ROUTE
-const refreshRoute = import.meta.env.VITE_REFRESH_ROUTE
-
-onMounted(async () => {
-    const sessionToken = localStorage.getItem('appname-session-token')
-    try {
-        const res = await fetch(authRoute, {
-            method: 'GET',
-            headers: { Authorization: `Bearer ${sessionToken}` },
-        })
-        if (res.status === 401) {
-            const refreshCheck = await fetch(refreshRoute, {
-                method: 'GET',
-                credentials: 'include',
-                headers: { ContentType: 'application/json' },
-            })
-            if (refreshCheck.status === 500) {
-                router.push('/login')
-            } else {
-                const jsonRes = await refreshCheck.json()
-                localStorage.setItem(
-                    'appname-session-token',
-                    jsonRes.sessionToken,
-                )
-            }
-        }
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('ERROR :=>', err)
-        }
-    }
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <!-- TODO: protect this route with auth/login via vue-router as well -->
