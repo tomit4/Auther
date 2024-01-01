4a1c15ba6b6cbd9f4c538def4675f17fc191573b
tomit4 <mosssap@gmail.com>
2024-01-01 05:12:03 -0800

    :memo: Made extensive note on poss jwt strat for v3

--- backend/dist/lib/routes/onboarding/auth.js
+++ backend/dist/lib/routes/onboarding/auth.js
@@ -7,6 +7,10 @@ exports.default = (fastify, options, done) => {
         url: '/auth',
         onRequest: fastify.authenticate,
         handler: async (request, reply) => {
+            /* NOTE: Technically our refresh token might be able to be authenticated within fastify.authenticate().
+             * This is essentially akin to HapiJS's authentication "strategies".
+             * Within fastify.authenticate(), we could check the refresh token
+             * and reissue our session token instead of through a separate refresh route. */
             return fastify.authenticate(request, reply);
         },
     });
--- backend/src/lib/routes/onboarding/auth.ts
+++ backend/src/lib/routes/onboarding/auth.ts
@@ -17,6 +17,10 @@ export default (
         url: '/auth',
         onRequest: fastify.authenticate,
         handler: async (request: FastifyRequest, reply: FastifyReply) => {
+            /* NOTE: Technically our refresh token might be able to be authenticated within fastify.authenticate().
+             * This is essentially akin to HapiJS's authentication "strategies".
+             * Within fastify.authenticate(), we could check the refresh token
+             * and reissue our session token instead of through a separate refresh route. */
             return fastify.authenticate(request, reply)
         },
     })
