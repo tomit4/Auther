3156ad3971e500c73dc557888d53ebe1748c1b9e
tomit4 <mosssap@gmail.com>
2024-01-16 05:07:00 -0800

    :wrench: Noob mistake, no dist folder in git

--- backend/dist/lib/services/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_service_1 = __importDefault(require("./user-service"));
-exports.default = async (fastify) => {
-    await fastify.register(user_service_1.default);
-};
