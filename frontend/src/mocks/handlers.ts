b92efb674e400531138fb15632ba7321ab959876
tomit4 <mosssap@gmail.com>
2024-01-16 07:42:47 -0800

    :pencil2: Replaced with proper route env

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,14 +1,13 @@
 import { http, HttpResponse } from 'msw'
 
 export const handlers = [
-    http.post(
-        'http://localhost:3000/onboarding/signup',
-        async ({ request }) => {
-            const { email } = await request.json()
-            return HttpResponse.json({
-                ok: true,
-                message: `Your Email Was Successfully Sent to ${email}`,
-            })
-        },
-    ),
+    http.post(process.env.VITE_EMAIL_ROUTE as string, async ({ request }) => {
+        // TODO: resolve TS error regarding 'DefaultBodyType'
+        // NOTE: probably just needs an extend or union
+        const { email } = await request.json()
+        return HttpResponse.json({
+            ok: true,
+            message: `Your Email Was Successfully Sent to ${email}`,
+        })
+    }),
 ]
