0cae4b331eb8bc7ac98acdface16bcd5f23aa76e
tomit4 <mosssap@gmail.com>
2024-01-17 03:55:24 -0800

    :goal_net: Fixed TS compiler errs regarding DefaultBodyType

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,10 +1,12 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse, type DefaultBodyType } from 'msw'
+
+type CustomBodyType = DefaultBodyType & {
+    email?: string
+}
 
 export const handlers = [
     http.post(process.env.VITE_EMAIL_ROUTE as string, async ({ request }) => {
-        // TODO: resolve TS error regarding 'DefaultBodyType'
-        // NOTE: probably just needs an extend or union
-        const { email } = await request.json()
+        const { email } = (await request.json()) as CustomBodyType
         return HttpResponse.json({
             ok: true,
             message: `Your Email Was Successfully Sent to ${email}`,
