0433f17c60c4664a358acc5eaf599db722c34c45
tomit4 <mosssap@gmail.com>
2024-01-18 07:45:23 -0800

    :recycle: Addressed TS compiler error

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,15 +1,22 @@
 /* v8 ignore next 17 */
+
+type Cookies = {
+    [key: string]: string
+}
+
 const delay = (ms: number): Promise<void> => {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 const grabStoredCookie = (cookieKey: string): string | undefined => {
-    const cookies = document.cookie.split('; ').reduce((prev, current) => {
-        const [key, ...value] = current.split('=')
-        // TODO: consider refactoring this using a Map
-        prev[key] = value.join('=')
-        return prev
-    }, {})
+    const cookies: Cookies = document.cookie
+        .split('; ')
+        .reduce((prev: Cookies, current) => {
+            const [key, ...value] = current.split('=')
+            // TODO: consider refactoring this using a Map
+            prev[key] = value.join('=')
+            return prev
+        }, {})
     const cookieVal = cookieKey in cookies ? cookies[cookieKey] : undefined
     return cookieVal
 }
