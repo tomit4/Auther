2ff100e3bc1a1cf1776e60d489703e03a33d8a81
tomit4 <mosssap@gmail.com>
2024-01-18 06:57:51 -0800

    :pencil2: Removed old commented out code

--- frontend/src/tests/VerifiedDeleteProfileView-success.test.ts
+++ frontend/src/tests/VerifiedDeleteProfileView-success.test.ts
@@ -13,28 +13,5 @@ describe('VerifiedDeleteProfileView', () => {
         )
         const errMessage = wrapper.find('[data-test="err-message"]')
         expect(errMessage.exists()).toBe(false)
-
-        // 'You have successfully deleted profile for jondoe@test.com, currently rerouting you...'
-        /*
-        expect(prompt.text()).toContain(
-            `Please enter your New Password for ${process.env.VITE_TEST_EMAIL}`,
-        )
-        await wrapper
-            .get('[data-test="password-input"]')
-            .setValue(process.env.VITE_TEST_PASSWORD)
-        await wrapper.get('[data-test="submit-btn"]').trigger('click')
-        await flushPromises()
-        const errMessage = wrapper.find('[data-test="err-message"]')
-        const resSuccessful = wrapper.get('[data-test="res-successful"]')
-        expect(errMessage.exists()).toBe(false)
-        expect(resSuccessful.text()).toContain(
-            'You have successfully changed your password!',
-        )
-        await flushPromises()
-        await wrapper.get('[data-test="submit-btn"]').trigger('click')
-        expect(prompt.text()).toContain(
-            `Please enter your New Password for ${process.env.VITE_TEST_EMAIL}`,
-        )
-        */
     })
 })
